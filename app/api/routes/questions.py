200a9cc901cfc7eb0b9e43f583018568a92ffdc4
hoangnm <nguyenminhhoang210891@gmail.com>
2024-07-11 20:15:38 +0700

    set middleware to get user id

--- app/api/routes/questions.py
+++ app/api/routes/questions.py
@@ -1,14 +1,16 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from pydantic import ConfigDict, BaseModel, Field
 import app.service.questions as questions_svc
 from typing_extensions import Annotated
 from pydantic.functional_validators import BeforeValidator
 from typing import Optional, List
+import logging
 
 router = APIRouter()
 
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
+logger = logging.getLogger(__name__)
 class QuestionResponseModel(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
     content: str = Field(...)
@@ -23,7 +25,10 @@ class QuestionResponsesCollection(BaseModel):
     questions: List[QuestionResponseModel]
 
 @router.get("/{quiz_id}", response_model=QuestionResponsesCollection)
-async def get_questions(quiz_id: str, limit: int = 10):
+async def get_questions(quiz_id: str, limit: int, request: Request):
+    # can use user_id for future use case
+    user_id = request.state.user_id
+    logger.info('user id: %i', user_id)
     if not quiz_id:
         return HTTPException(status_code=400, detail="quiz_id is required")
     questions = await questions_svc.get_questions(quiz_id, limit)
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request
 from app.api.main import api_router
 import logging
 
@@ -13,6 +13,14 @@ logging.basicConfig(
 
 app = FastAPI()
 app.include_router(api_router)
+
+
+@app.middleware("http")
+async def auth_middleware(request: Request, call_next):
+    # This is a simple example of how you can use middleware to authenticate users. We can swap to use jwt decode here
+    request.state.user_id = 1
+    response = await call_next(request)
+    return response
 
 if __name__ == "__main__":
     import uvicorn
